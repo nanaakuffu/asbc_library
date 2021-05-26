9273a7f38f591da1177f5a8c5e90c0416993c90d
Nana Baah Akuffu <nanaakuffu@gmail.com>
2021-05-26 22:32:45 +0000

    Initial commit

---
+++ mydev/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\niilaatey\documents\asbc library system\mydev\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
